4bb8e91c375839a598376396a8c28a778422d7ec
Sevag <sevagared2002@gmail.com>
2021-11-27 11:39:11 -0500

    Fully finished the shopkeeping functionality

---
+++ Assets/Scripts/ShopItem.cs
@@ -0,0 +1,14 @@
+public class ShopItem
+{
+    // the image will have the same name
+    public string name { get; set; }
+    public string description { get; set; }
+    public int price { get; set; }
+    public float healthAmount { get; set; }
+
+    public ShopItem(/**string name, string description, int price*/) {
+        // this.name = name;
+        // this.description = description;
+        // this.price = price;
+    }
+}
